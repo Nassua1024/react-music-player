46f43e1bdbb84059cf19974674bbfb77b69a8114
Nassua <562408405@qq.com>
2018-03-26 18:19:19 +0800

    update drag

--- src/views/index.jsx
+++ src/views/index.jsx
@@ -10,10 +10,10 @@ class Index extends Component {
         super(props);
         this.state = {
             isPlaying: false, // 进入页面音乐正在是否播放 用于控制 needle 动画
-            duration: '--', // 歌曲总时间
-            currentTime: '--', // 当前播放时间
-            progressRate: 0, // 播放进度长度
+            duration: 0, // 歌曲总时间
+            currentTime: 0, // 当前播放时间
             isPlay: false, // 是否播放状态
+            isDrag: false
         };
     }
 
@@ -22,51 +22,68 @@ class Index extends Component {
         this.timeUpdate();
     }
 
-    // 拖拽播放进度
-    handleMove(e) {
-
-        const offsetWidth = this.refs.progress.offsetWidth; // 进度条长度
-        const barWidth = this.refs.bar.offsetWidth; // 小圆圈长度
-        const clientX = this.refs.progress.offsetLeft; // 进度条距离页面左边距离
-        const pageX = e.changedTouches[0].pageX; // 手指位置
-        let progressRate = (pageX - clientX) / offsetWidth * 100; // 当前播放进度百分比
-        const shouldMoveTotalRate = (1 - barWidth / offsetWidth) * 100; // 小圆圈最长可移动百分比
-
-        if(progressRate <= 0) progressRate = 0;
-        if(progressRate >= shouldMoveTotalRate) progressRate = shouldMoveTotalRate;
-
-        this.setState({ progressRate });
-    }
-
     // 播放音乐
     playMusic() {
-       
+        
         const { isPlay } = this.state;
-       
+        
         this.setState({ 
             isPlay: !isPlay, 
-            initPage: true
+            isPlaying: true
         }, !isPlay ? () => this.refs.audio.play() : () => this.refs.audio.pause() );
     }
 
     // 获取播放时间 播放进度
     timeUpdate() {
-
+        
         const _this = this.refs.audio;
 
         this.refs.audio.addEventListener('timeupdate', () => {
-            
-            const duration = new Date(parseInt(_this.duration) * 1000).Format('mm:ss');
-            const currentTime = new Date(parseInt(_this.currentTime *  1000)).Format('mm:ss');
-            const progressRate = _this.currentTime/_this.duration * 100;
 
-            this.setState({ duration, currentTime, progressRate });
+            if(!this.state.isDrag) {
+               
+                const duration = parseInt(_this.duration) * 1000;
+                const progressRate = _this.currentTime/_this.duration * 100;
+                const currentTime = parseInt(_this.currentTime *  1000);
+                   
+                this.setState({ duration, currentTime, progressRate });
+            }
         });
+    }
+
+    // 拖拽播放进度
+    handleMove(e) {
+
+        const { duration } = this.state;
+        const offsetWidth = this.refs.progress.offsetWidth; // 进度条长度
+        const clientX = this.refs.progress.offsetLeft; // 进度条距离页面左边距离
+        const pageX = e.changedTouches[0].pageX; // 手指位置
+        let progressRate = (pageX - clientX) / offsetWidth * 100; // 当前播放进度百分比
+
+        if(progressRate <= 0) progressRate = 0;
+        if(progressRate >= 100) progressRate = 100;
+
+        const currentTime = parseInt(progressRate / 100 * duration);
+
+        this.setState({ 
+            progressRate, 
+            currentTime,
+            isDrag: true
+        });
+    }
+
+    // 拖拽播放进度结束
+    handleEnd() {
+
+        const { currentTime } = this.state;
+        
+        this.refs.audio.currentTime = currentTime / 1000;
+        this.setState({ isDrag: false });
     }
 
     render() {
 
-        const { initPage, duration, currentTime, progressRate, isPlay } = this.state;
+        const { isPlaying, duration, currentTime, progressRate, isPlay } = this.state;
 
         return (
             <div className="index-wrap">
@@ -80,18 +97,25 @@ class Index extends Component {
                 </header>
                 <div className="play-wrap">
                     <i><span></span></i>
-                    <img className={ isPlay ? 'play needle' : initPage ? 'pause needle' : 'needle' } src={ require('@/assets/images/needle.png') } alt="" />
+                    <img className={ isPlay ? 'play needle' : isPlaying ? 'pause needle' : 'needle' } src={ require('@/assets/images/needle.png') } alt="" />
                     <div className={ isPlay ? 'play disc': 'disc' }>
                         <img src={ require('@/assets/images/878431574282626913.jpg') } alt="" />
                     </div>
                 </div>
                 <div className="progress-wrap">
-                    <span>{ currentTime }</span>
+                    <span>{ new Date(currentTime).Format('mm:ss') }</span>
                     <div className="time" ref="progress">
                         <div className="current-time" style={ { 'width': `${progressRate}%` } }></div>
-                        <i ref="bar" onTouchMove={ e => this.handleMove(e) } style={{ 'left': `${progressRate}%`}}><em></em></i>
+                        <i 
+                            ref="bar" 
+                            style={{ 'left': `${progressRate}%`}}
+                            onTouchMove={ e => this.handleMove(e) } 
+                            onTouchEnd={ () => this.handleEnd() }                            
+                        >
+                            <em></em>
+                        </i>
                     </div>
-                    <span>{ duration }</span>
+                    <span>{ new Date(duration).Format('mm:ss') }</span>
                 </div>
                 <div className="play-btn">
                     <a href="javascript: ">
